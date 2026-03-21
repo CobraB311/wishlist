6959690fcee66cd9efcb40c2d35b0420438302e8
Ruben Bernaert <bernaertruben@hotmail.com>
2026-03-21 17:42:57 +0100

    New config for google link for PRO

---
+++ kids/config.js
@@ -0,0 +1,4 @@
+// CONFIGURATIE - Pas de URL aan per omgeving (DEV/PRO)
+const CONFIG = {
+    GOOGLE_SHEET_URL: 'https://script.google.com/macros/s/AKfycbzGvDwK8roiwK45mtoyC-rsNEv2vXEwvZ4en7X8n3QXfPeGLW1X2pLdf23tMFu8BlJGVQ/exec'
+};

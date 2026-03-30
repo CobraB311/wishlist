66a9f1987d6d06715c3a56c0a2f2ed2d08feac8d
Ruben Bernaert <bernaertruben@hotmail.com>
2026-03-30 18:45:23 +0200

    New google app script with mailing

--- kids/dev/paw-patrol/config.js
+++ kids/dev/paw-patrol/config.js
@@ -1,4 +1,4 @@
 // CONFIGURATIE - Pas de URL aan per omgeving (DEV/PRO)
 const CONFIG = {
-    GOOGLE_SHEET_URL: 'https://script.google.com/macros/s/AKfycbygEM-gdECfVAf_M-KiUjt06r2mWwwvsZrOqGIbz5R-zL0C5eFc9twgexbsfXNSegNQ/exec'
+    GOOGLE_SHEET_URL: 'https://script.google.com/macros/s/AKfycbyesG71CN4A12ycpKgzTDw7V96x_55PDT3iYy13vGQPeNpxuIhed0w3pASfTObzD_dD/exec'
 };

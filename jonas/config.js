0ebc161b3481464830f5f932156cea973a00ffb5
Ruben Bernaert <bernaertruben@hotmail.com>
2026-04-18 16:48:03 +0200

    PRO Release

---
+++ jonas/config.js
@@ -0,0 +1,4 @@
+// CONFIGURATIE - Pas de URL aan per omgeving (DEV/PRO)
+const CONFIG = {
+    GOOGLE_SHEET_URL: 'https://script.google.com/macros/s/AKfycbz7PvIjoK3cntElZNPuSGKeWCs1n1g7JiF_SEee4XtzXU4wgtFh6vdpcfvKk_Ezqm6jOA/exec'
+};
